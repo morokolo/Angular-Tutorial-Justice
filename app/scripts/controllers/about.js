9d139c84184bfaea7ea20135a34243c73439f8ea
justice <justice@tangentsolutions.co.za>
2016-08-16 10:08:41 +0200

    --application scaffolding using yeomen

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutorialJusticeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTutorialJusticeApp
+ */
+angular.module('angularTutorialJusticeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutorialJusticeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTutorialJusticeApp
+ */
+angular.module('angularTutorialJusticeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
